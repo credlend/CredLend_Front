3f03976113654946ddff2ef62e87c3ffe84f4aa3
Igor <igorbaroni77@gmail.com>
2023-10-20 14:17:48 -0300

    painel-controle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PainelcontroleComponent } from './painelcontrole/painelcontrole.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelcontroleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/painelcontrole/painelcontrole.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-painelcontrole',
+  templateUrl: './painelcontrole.component.html',
+  styleUrls: ['./painelcontrole.component.css']
+})
+export class PainelcontroleComponent {
+
+}
