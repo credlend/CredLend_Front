b46221ee755762f1dba3dbd812cca980c0250ef4
Igor <igorbaroni77@gmail.com>
2023-10-23 14:57:20 -0300

    'responsive'

--- src/app/painelcontrole/painelcontrole.component.ts
+++ src/app/painelcontrole/painelcontrole.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-painelcontrole',
   templateUrl: './painelcontrole.component.html',
-  styleUrls: ['./painelcontrole.component.css']
+  styleUrls: ['./painelcontrole.component.css', './painelcontrole.component.responsive.css']
 })
 export class PainelcontroleComponent {
 
