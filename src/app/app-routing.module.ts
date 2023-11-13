d5a2107375f75b9203c90ae0c00813ed38e4387a
Cauan7267 <cauan.marioto@gmail.com>
2023-11-13 09:32:48 -0300

    telas de login e cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PainelcontroleComponent } from './features/painelcontrole/painelcontrole.component';
 import { HomeComponent } from './features/home/home.component';
+import { TelaLoginComponent } from './features/tela-login/tela-login.component';
+import { TelaCadastroComponent } from './features/tela-cadastro/tela-cadastro.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'painelcontrole', component: PainelcontroleComponent },
+    { path: 'login', component: TelaLoginComponent },
+    { path: 'cadastro', component: TelaCadastroComponent },
   ];
   
   @NgModule({
