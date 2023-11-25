06d972b3bbbaf76556c941871000e5de7cadb6d6
Igor <igorbaroni77@gmail.com>
2023-11-25 16:21:57 -0300

    'formcontrol'

--- src/app/features/painelcontrole/painelcontrole.component.ts
+++ src/app/features/painelcontrole/painelcontrole.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-painelcontrole',
@@ -10,11 +11,35 @@ export class PainelcontroleComponent {
   public nome = "Leonardo";
   public saldo = 0;
   public mostrarSaldo = false;
+  formLoan!: FormGroup;
 
+  constructor(private fb: FormBuilder) 
+  {
+    this.createFormLoan();
+  }
 
+  createFormLoan() {
+    this.formLoan = this.fb.group({
+      id: [''],
+      typePlan: [''],
+      valuePlan: ['', [Validators.required]],
+      transactionWay: [''], 
+      userID: [''], 
+      userName: [''], 
+      email: [''], 
+      operationDate: ['', [Validators.required]],
+      isActive: [''],
+      paymentTerm: [''],
+      interestRate: ['', [Validators.required]] 
+    });
+  }
+
+  SubmitLoan(){}
 
   esconderMostrar(){
     this.mostrarSaldo = !this.mostrarSaldo;
   }
+
+
 
 }
